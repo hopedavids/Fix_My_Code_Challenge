2c9d2cfaba2fbef812ff4654e0cb30b31dba8f4d
Hope_Davids <hopedavids@gmail.com>
2023-05-18 09:07:44 +0000

    modify the square method

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 
-class square():
-    
-    width = 0
-    height = 0
-
-    
+class Square:
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
+    def area(self):
+        """ Calculate the area of the square """
+        return self.width ** 2
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
+        """ Calculate the perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
+
